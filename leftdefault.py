e67245a19eaf56534fb49247e1ddf0c812a70577
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-04-16 07:14:22 -0700

    Add type hints (#3)
    Signed-off-by: nstarman <nathanielstarkman@gmail.com>
    Co-authored-by: Nicolas Tessore <n.tessore@ucl.ac.uk>

--- leftdefault.py
+++ leftdefault.py
@@ -1,59 +1,90 @@
 # license: MIT
 '''Support for default arguments on the left'''
 
-__version__ = '2023.4.15'
+__version__ = '2023.4.16'
 
 __all__ = ['leftdefault']
 
-
 from functools import wraps, cached_property, partial
-from inspect import Signature as _Signature
+from inspect import Parameter, Signature as _Signature
 from types import MappingProxyType
+from typing import Any, Callable, Tuple, TypeVar, overload
+
+F = TypeVar('F', bound=Callable[..., Any])
 
 
 class Signature(_Signature):
-    left_default_skip = 0
+    '''Signature that reverses the order of positional-only arguments.
+
+    This is a workaround for the fact that default arguments must be on
+    the right.
+
+    '''
+
+    left_default_skip: int = 0
 
     @cached_property
-    def left_default_splits(self):
-        i, j = self.left_default_skip, None
+    def left_default_splits(self) -> Tuple[int, int, int]:
+        '''Take the list of positional-only arguments and return indices
+        i, j, k such that
+            - args[:i] are skipped over,
+            - args[i:j] are required, and
+            - args[j:k] are optional.
+        '''
+        i = k = self.left_default_skip
+        j = -1
         for k, par in enumerate(super().parameters.values()):
             if k < i:
                 continue
             if par.kind != par.POSITIONAL_ONLY:
                 break
-            if par.default != par.empty and j is None:
+            if par.default != par.empty and j == -1:
                 j = k
-        if j is None:
+        if j == -1:
             j = k
         if i > k:
             i = k
         return i, j, k
 
     @cached_property
-    def parameters(self):
+    def parameters(self) -> MappingProxyType[str, Parameter]:
+        '''Reordered parameters list with left-defaults.'''
         pars = super().parameters
         names = list(pars.keys())
         i, j, k = self.left_default_splits
         return MappingProxyType({name: pars[name]
-                                 for name in [*names[:i], *names[j:k],
-                                              *names[i:j], *names[k:]]})
+                                 for name in (*names[:i], *names[j:k],
+                                              *names[i:j], *names[k:])})
 
 
-def leftdefault(func=None, *, skip=0):
+@overload
+def leftdefault(func: F, /, *, skip: int = 0) -> F:
+    ...
+
+
+@overload
+def leftdefault(func: None, /, *, skip: int = 0) -> Callable[[F], F]:
+    ...
+
+
+def leftdefault(func: Any, /, *, skip: Any = 0) -> Any:
+    '''Move positional-only default arguments to the left.'''
     if func is None:
         return partial(leftdefault, skip=skip)
+
+    if not callable(func):
+        raise TypeError('func must be callable')
 
     sig = Signature.from_callable(func)
     sig.left_default_skip = skip
     i, j, k = sig.left_default_splits
-    n = j-i
+    n = j - i
 
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
         skip, pos, rest = args[:i], args[i:k], args[k:]
         return func(*skip, *pos[-n:], *pos[:-n], *rest, **kwargs)
 
-    wrapper.__signature__ = sig
+    wrapper.__signature__ = sig  # type: ignore[attr-defined]
 
     return wrapper
