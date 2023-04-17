5a521087e2d5099b1a576b36b42315fd1382202e
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-04-17 21:02:10 +0100

    optional build with mypyc (#7)

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 # type: ignore
 
+import sys
+import os
 from setuptools import setup
 
-setup(py_modules=[
-    'positional_defaults',
-    '_positional_defaults',
-])
+py_modules = ['positional_defaults', '_positional_defaults']
+ext_modules = []
+setup_requires = []
+
+use_mypyc = False
+
+if '--use-mypyc' in sys.argv:
+    sys.argv.remove('--use-mypyc')
+    use_mypyc = True
+elif os.getenv('USE_MYPYC', None) is not None:
+    use_mypyc = True
+
+if use_mypyc:
+    try:
+        from mypyc.build import mypycify
+    except ModuleNotFoundError:
+        setup_requires.append('mypy')
+    else:
+        py_modules.remove('positional_defaults')
+        ext_modules = mypycify(['positional_defaults.py'])
+
+setup(py_modules=py_modules,
+      ext_modules=ext_modules,
+      setup_requires=setup_requires)
