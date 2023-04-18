05af942b10631d01f965f783543bba034f301fe5
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-04-18 08:28:51 +0100

    correctly initialise Signature class

--- _positional_defaults.py
+++ _positional_defaults.py
@@ -1,11 +1,16 @@
 '''Implementation details of the positional_defaults package.'''
 
 from inspect import Parameter, Signature as _Signature
+from typing import Any
 from types import MappingProxyType
 
 
 class Signature(_Signature):
     '''Signature with defaults anywhere in positional-only parameters.'''
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.__parameters = super().parameters
 
     def update_defaults(self, **defaults: object) -> None:
         '''Set default values anywhere in the positional-only parameters.'''
@@ -18,8 +23,8 @@ class Signature(_Signature):
             if par.kind != par.POSITIONAL_ONLY:
                 raise ValueError(f'parameter "{name}" is not positional-only')
             parameters[name] = par.replace(default=default)
-        self.__parameters = parameters
+        self.__parameters = MappingProxyType(parameters)
 
     @property
     def parameters(self) -> MappingProxyType[str, Parameter]:
-        return MappingProxyType(self.__parameters)
+        return self.__parameters
